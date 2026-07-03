3f52649d271ec913ea26e2adfadf1d68c4f0ecda
a20231333-prog <a20231333@pucp.edu.pe>
2026-07-03 18:51:44 -0500

    Update PC3.py

--- PC3.py
+++ PC3.py
@@ -186,7 +186,7 @@ elif selected == 'Gráficos':
 
         with col7:
             st.image(
-                "lengua_familia_GB.png",
+                "BARRA.png",
                 width=800
             )
     elif grafico_seleccionado == 'Mapa_1':
